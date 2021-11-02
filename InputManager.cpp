807b2d6d91f949eba2ca5d41ca2daf338bcb0a03
arnebesp <arnebesp2531@uwec.edu>
2021-11-02 16:29:14 -0500

    implemented getAlgorithmType

--- InputManager.cpp
+++ InputManager.cpp
@@ -8,9 +8,15 @@ void InputManager::setAlgorithmType(int input) {
     algorithmType = input;
 };
 
+int InputManager::getAlgorithmType() {
+    return algorithmType;
+}
+
 int main() {
     InputManager input;
 
     input.setAlgorithmType(2);
+    cout << input.getAlgorithmType();
+
     return 0;
 };
