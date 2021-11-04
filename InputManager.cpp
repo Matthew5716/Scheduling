e39bc2c64fef2df06a89592b58e5dc9a4c376515
Wei Zhong Tee <weizhong.tee@jamf.com>
2021-11-03 21:41:13 -0500

    Add Input checking

--- InputManager.cpp
+++ InputManager.cpp
@@ -3,8 +3,26 @@
 #include "InputManager.h"
 using namespace std;
 
-void InputManager::setAlgorithmType(int input) {
-    cout << "done" << endl;
+void InputManager::welcomeMessage() {
+    cout << "****** Welcome to CS452's Lab 2: Process Scheduling Assignment. ******" << endl;
+    cout << "You will be asked to choose running options below, please only enter inputs 1 or 0. " << endl << endl;
+}
+
+void InputManager::setAlgorithmType() {
+    cout << "Choose the algorithm you would like to run for process scheduling:" << endl;
+    cout << "Enter \'0\' for a MFQS algorithm, and enter \'1\' for a real time algorithm" << endl;
+    cout << "Input your choice here: ";
+    int input;
+    cin >> input;
+    
+    if (!goodInput(input)) {
+        while (!goodInput(input)) {
+            cout << "Please only input values 1 and 0." << endl;
+            cout << "Enter \'0\' for a MFQS algorithm, and enter \'1\' for a real time algorithm" << endl;
+            cout << "Input your choice here: ";
+            cin >> input;
+        }
+    }
     algorithmType = input;
 };
 
@@ -12,11 +30,18 @@ int InputManager::getAlgorithmType() {
     return algorithmType;
 }
 
+bool InputManager::goodInput(int input) {
+    if(input == 1 || input == 0) {
+        return true;
+    }
+    return false;
+}
+
 int main() {
     InputManager input;
-
-    input.setAlgorithmType(2);
-    cout << input.getAlgorithmType();
+    input.welcomeMessage();
+    input.setAlgorithmType();
+    cout << input.getAlgorithmType() << endl;
 
     return 0;
 };
