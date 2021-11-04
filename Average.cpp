36f008bfec4f1a8e1a51282e5e7a76cc32b0a181
Matthew Martinez <martinma5716@uwec.edu>
2021-11-04 15:09:45 -0500

    Add Average class

---
+++ Average.cpp
@@ -0,0 +1,27 @@
+#include "Average.h";
+
+
+double Average::getAverageWaitTime() {
+    if(numProcesses == 0) {
+        return 0;
+    }
+    return totalWaitTime / numProcesses;
+}
+
+double Average::getAverageTurnAroundTime() {
+    if(numProcesses == 0) {
+        return 0;
+    }
+    return totalTurnAroundTime / numProcesses;
+
+}
+void Average::addProcessToAverages(Process& process) {
+    int waitTime = process.getWaitTime();
+    int turnAroundTime = process.getTurnaroundTime();
+    if(waitTime != -1 && turnAroundTime != -1) {
+        numProcesses++;
+        totalWaitTime += waitTime;
+        turnAroundTime += turnAroundTime;
+    }
+}
+
